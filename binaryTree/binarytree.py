d405421547802baf3f81049fe746274b47c4a7b2
oscar1234456 <chen.oscar@hotmail.com>
2020-05-09 11:24:38 +0800

    [Modify]Add the comment in file

--- binaryTree/binarytree.py
+++ binaryTree/binarytree.py
@@ -14,18 +14,24 @@ class treeNode(object):
         self.data = val
         self.leftNode = None
         self.rightNode = None
+    
+    #let this node has Left child
     def addLeftNode(self, node):
         self.leftNode = node
+
+    #let this node has Right child    
     def addRightNode(self, node):
         self.rightNode = node
+
+    #Use LVR Tree Traversal    
     def viewTree(self,root):
         if root == None:
             return
-        self.viewTree(root.leftNode)
-        print(root.data)
-        self.viewTree(root.rightNode)
+        self.viewTree(root.leftNode) # use recursion #L
+        print(root.data) #print the data in node #V
+        self.viewTree(root.rightNode) # use recursion #R
         
-    
+#Generate 5 nodes object   
 root = treeNode(0)
 node1 = treeNode(1)
 node2 = treeNode(2)
@@ -38,10 +44,11 @@ root.addRightNode(node2)
 
 #level3
 node1.addLeftNode(node3)
+
 #level4
 node3.addRightNode(node4)
 
-
+#Start Tree Traversal
 root.viewTree(root)
 
 
